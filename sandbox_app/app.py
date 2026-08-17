5267dcbe405e07bf1a9709d1b26932edc8a53a0c
Akash <aakashbajpai2003@gmail.com>
2026-08-17 10:56:57 +0530

    just added a Flask skeleton

---
+++ sandbox_app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/unit/search")
+def search():
+    name = request.args.get("name", "")
+    return f"Searching for unit: {name}"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
